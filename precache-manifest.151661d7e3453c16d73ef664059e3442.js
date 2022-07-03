2557f4c423413578780fd5974066106292d2e194
radonex1984 <mihaila.radu.gabriel@gmail.com>
2022-07-03 09:40:53 +0300

    Updates

---
+++ precache-manifest.151661d7e3453c16d73ef664059e3442.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfa4f06fedf7bcff5bde",
+    "url": "/my-radio/static/css/main.a98d93b1.chunk.css"
+  },
+  {
+    "revision": "dfa4f06fedf7bcff5bde",
+    "url": "/my-radio/static/js/main.79d5b62d.chunk.js"
+  },
+  {
+    "revision": "c93df563159991b677a6",
+    "url": "/my-radio/static/js/runtime~main.17cfe6c7.js"
+  },
+  {
+    "revision": "9ceae406e41001d258e9",
+    "url": "/my-radio/static/css/2.7c4e3637.chunk.css"
+  },
+  {
+    "revision": "9ceae406e41001d258e9",
+    "url": "/my-radio/static/js/2.548d5568.chunk.js"
+  },
+  {
+    "revision": "9dc639999e908e298750",
+    "url": "/my-radio/static/js/3.1090266c.chunk.js"
+  },
+  {
+    "revision": "d65ff20ef74b013e1d1e8dbca450aa6b",
+    "url": "/my-radio/static/media/radio.d65ff20e.png"
+  },
+  {
+    "revision": "9b53c6c961189b815d76820e9cc2d763",
+    "url": "/my-radio/static/media/logo.9b53c6c9.png"
+  },
+  {
+    "revision": "6e7c276fdb901506f6cb0d8d6bf3bd55",
+    "url": "/my-radio/index.html"
+  }
+];
